01b5182b4a52e9fcdde47c319b9c687efa495caf
we5046 <76059661+we5046@users.noreply.github.com>
2026-04-19 17:19:35 +0900

    Study: 문ì변수 작성 시, 소문자 시작, camelCase 사용 (#2)
    Co-authored-by: sunghoon <cook5046@gmail.com>

---
+++ java-practice/src/basic/variable/NamingExample.java
@@ -0,0 +1,13 @@
+package basic.variable;
+
+public class NamingExample {
+    static void main() {
+        int userAge = 25;
+        boolean isActive = true;
+        final int MAX_SIZE = 100;
+
+        System.out.println("User Age: " + userAge);
+        System.out.println("Is Active: " + isActive);
+        System.out.println("Max Size: " + MAX_SIZE);
+    }
+}
---
+++ java-practice/src/basic/variable/VariableEx1.java
@@ -0,0 +1,24 @@
+package basic.variable;
+
+public class VariableEx1 {
+    static void main() {
+        // 기본형 byte << short << int << long | float << double
+        byte byteValue = Byte.MAX_VALUE;
+        System.out.println(byteValue);
+
+        short shortValue = Short.MAX_VALUE;
+        System.out.println(shortValue);
+
+        int intValue = Integer.MAX_VALUE;
+        System.out.println(intValue);
+
+        long longValue = Long.MAX_VALUE;
+        System.out.println(longValue);
+
+        float floatValue = Float.MAX_VALUE;
+        System.out.println(floatValue);
+
+        double doubleValue = Double.MAX_VALUE;
+        System.out.println(doubleValue);
+    }
+}
